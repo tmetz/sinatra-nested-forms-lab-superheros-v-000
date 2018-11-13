0475b930fae1055a424a126c3a6cc77fc6ab54a2
Tammy Metz <tammy@tammymetz.com>
2018-11-13 22:35:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,13 @@ class App < Sinatra::Base
       erb :superheroform
     end
 
+    post "/teams" do
+      @team = Team.new(params[:team])
+      params[:team][:heros].each do |hero_info|
+        Hero.new(hero_info)
+      end
+      @heroes = Hero.all
+      erb :team
+    end
+
 end
